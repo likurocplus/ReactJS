5aee7b8359d1c732cdd4ce30037c0525fdae89a4
Khoi Quoc <likuroquoc@gmail.com>
2024-10-03 20:28:21 +0700

    add lecture 2

--- lecture-02-create-react-app/src/index.js
+++ lecture-02-create-react-app/src/index.js
@@ -1,13 +1,56 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 
 function App() {
-    return <h1>Hello React</h1>;
+  return <h1>Hello React</h1>;
+}
+
+function Rudius() {
+  const name = "Rudius";
+  return (
+    <div>
+      <p>My name is {name}</p>
+      <img src="https://i.pinimg.com/736x/07/c0/1c/07c01cf6c639d15da3cd864a6d3bb504.jpg" />
+      <button
+        style={{
+          borderRadius: "20px",
+          backgroundColor: "blue",
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
+}
+
+function Avatar({ person, size }) {
+  return (
+    <>
+      <h1>My name is {person.name}</h1>
+      <img
+        style={{
+          width: { size },
+          height: "auto",
+        }}
+        className="avatar"
+        src={person.avatar} //Curly Braces //string is "{avatar}" => empty image
+      />
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <Rudius />
+    <Avatar
+      person={{
+        name: "Rudius",
+        avatar:
+          "https://i.pinimg.com/736x/07/c0/1c/07c01cf6c639d15da3cd864a6d3bb504.jpg",
+      }}
+      size={100}
+    />
+  </React.StrictMode>
 );
